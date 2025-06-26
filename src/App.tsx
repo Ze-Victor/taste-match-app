b01ca603d5ad1a7ed2784e7d5b538a2a29992ebe
Teteu <mattahyah@gmail.com>
2025-06-26 14:00:14 -0300

    feat: initialize project with Vite + React, ESLint, and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex w-full border items-center justify-center text-5xl">
+        <h1 className="font-bold">Taste Match</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
