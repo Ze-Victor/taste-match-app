322beecb8cb55789d73e5c14819d45e2ea470524
andersonrocha <ironmanrocha81@gmail.com>
2025-07-08 02:31:01 -0300

    feat: create form fields

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,42 +1,104 @@
 export default function Formulario() {
     return (
-        <div className="w-full max-w-xs">
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                        Nome
-                    </label>
-                    <input
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="username" type="text" placeholder="Nome"/>
+        <div className="w-full max-w-xl">
+            <form className="w-full max-w-xl bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div className="flex flex-wrap -mx-3 mb-6">
+                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                               htmlFor="nome">
+                            Nome
+                        </label>
+                        <input
+                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                            id="nome" type="text" placeholder="Nome"/>
+                        {/*<p className="text-red-500 text-xs italic">Please fill out this field.</p>*/}
+                    </div>
+                    <div className="w-full md:w-1/2 px-3">
+                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                               htmlFor="email">
+                            Email
+                        </label>
+                        <input
+                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="email" type="email" placeholder="Email"/>
+                    </div>
                 </div>
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                        Email
-                    </label>
-                    <input
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="username" type="email" placeholder="Email"/>
+                <div className="flex flex-wrap -mx-3 mb-6">
+                    <div className="w-full px-3">
+                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                               htmlFor="senha">
+                            Senha
+                        </label>
+                        <input
+                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="senha" type="password" placeholder="******************"/>
+                        {/*<p className="text-gray-600 text-xs italic"></p>*/}
+                    </div>
                 </div>
-                <div className="mb-6">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                        Senha
-                    </label>
-                    <input
-                        className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                        id="password" type="password" placeholder="******************"/>
-                    <p className="text-red-500 text-xs italic">Escolha uma senha.</p>
+                <div className="flex flex-wrap -mx-3 mb-2">
+                    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                               htmlFor="sexo">
+                            Sexo
+                        </label>
+                        <div className="relative">
+                            <select
+                                className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                id="sexo">
+                                <option>Masculino</option>
+                                <option>Feminino</option>
+                            </select>
+                            <div
+                                className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                     viewBox="0 0 20 20">
+                                    <path
+                                        d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                               htmlFor="gastronomia">
+                            Gastronomia
+                        </label>
+                        <div className="relative">
+                            <select
+                                className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                id="gastronomia">
+                                <option>Brasileira</option>
+                                <option>Japonesa</option>
+                                <option>Italiana</option>
+                                <option>Mexicana</option>
+                                <option>Chinesa</option>
+                            </select>
+                            <div
+                                className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                     viewBox="0 0 20 20">
+                                    <path
+                                        d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                               htmlFor="nascimento">
+                            Nascimento
+                        </label>
+                        <input
+                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="nascimento" type="date"/>
+                    </div>
                 </div>
-                <div className="flex items-center justify-between">
+                <div className="justify-self-center">
                     <button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded focus:outline-none focus:shadow-outline"
                         type="button">
                         Cadastrar
                     </button>
-                    <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-                       href="#">
-                        Esqueceu a senha?
-                    </a>
                 </div>
             </form>
             <p className="text-center text-gray-500 text-xs">
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,12 @@
 export default function Header() {
     return (
-        <nav className="w-full mb-20 flex bg-white p-4 place-content-center">
-            <div className="flex items-center flex-shrink-0 text-white mr-6">
-                <span className="text-gray-700 font-semibold text-4xl tracking-tight">Taste Match</span>
-            </div>
-            <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto">
-                <div>
-                    <button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        type="button">
-                        Entrar
-                    </button>
-                </div>
-            </div>
-        </nav>
+        <div className="flex place-content-around w-full mb-20 bg-white p-4 text-white">
+            <span className="text-gray-700 font-semibold text-4xl tracking-tight">Taste Match</span>
+            <button
+                className="lg:flex lg:items-center lg:w-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button">
+                Entrar
+            </button>
+        </div>
     );
 }
